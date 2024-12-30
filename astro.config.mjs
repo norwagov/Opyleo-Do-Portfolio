d4664c453fd76e223ae6c05e047ed0e720e82c55
grzes-business <grzes.business@gmail.com>
2024-12-30 19:42:25 +0100

    Removing the base from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://grzes-business.github.io',
-  base: 'Opyleo-Landing-Page',
+  site: 'https://grzes-business.github.io/Opyleo-Landing-Page',
   integrations: [tailwind()],
   vite: {
     ssr: {
