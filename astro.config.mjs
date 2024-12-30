1ee6d4ad675725d84a4048a4bea486da7ae0794c
grzes-business <grzes.business@gmail.com>
2024-12-30 19:50:43 +0100

    Reverting to using base again because of render issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://grzes-business.github.io/Opyleo-Landing-Page',
+  site: 'https://grzes-business.github.io',
+  base: 'Opyleo-Landing-Page',
   integrations: [tailwind()],
   vite: {
     ssr: {
