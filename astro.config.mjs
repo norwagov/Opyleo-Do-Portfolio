55d8af5a1b9dd4e3afd830428d62f2e060884cd6
grzes-business <grzes.business@gmail.com>
2025-01-28 10:07:07 +0100

    Upgraded astro to 5.1.0. Introduced supposedly required jsx configurations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import solidJs from '@astrojs/solid-js';
+
 export default defineConfig({
   site: 'https://grzes-business.github.io',
   base: 'Opyleo-Landing-Page',
-  integrations: [tailwind()],
+  integrations: [tailwind(), solidJs()],
   vite: {
     ssr: {
       noExternal: ['swiper', 'swiper/*']
